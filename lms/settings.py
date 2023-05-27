40797dfc50d1971ab2ea5c9818208b8ab2f31a76
AdnanFarooque <adnan.dipta89@gmail.com>
2023-05-27 16:21:03 +0600

    Updated settings.py

--- lms/settings.py
+++ lms/settings.py
@@ -78,11 +78,15 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'lms',
+    #     'USER': 'dipta',
+    #     'PASSWORD':'1234',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'lms',
-        'USER': 'dipta',
-        'PASSWORD':'1234',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
 
