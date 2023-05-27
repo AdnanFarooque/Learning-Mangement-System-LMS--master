6bf8ca812b4b97fe819c3a41332c722050b9be1e
AdnanFarooque <adnan.dipta89@gmail.com>
2023-05-27 17:48:05 +0600

    Updated a bit

--- lms/settings.py
+++ lms/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')7gno0)u%l2-(b3-1u3h)-@nra728h*2i-vh()=(3yym8&e2e4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
 
 # Application definition
