e6d37dc4f76e31cc0d7745d59b45480151e86a33
AdnanFarooque <adnan.dipta89@gmail.com>
2023-05-28 11:17:22 +0600

    Reversed settings.py

--- lms/settings.py
+++ lms/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import mimetypes
 
+mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -155,5 +156,3 @@ INSTALLED_APPS += (
     'django_wysiwyg',
     'form_designer',
 )
-
-mimetypes.add_type("text/css", ".css", True)
